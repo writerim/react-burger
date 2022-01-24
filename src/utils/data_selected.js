a187ed5f4ec8ddd80605543389296f7a3c65568b
Ivanov Sergey <Ivanov.S@citilink.ru>
2022-01-24 20:06:33 +0300

    верхняя и нижняя булка

--- src/utils/data_selected.js
+++ src/utils/data_selected.js
@@ -70,19 +70,19 @@ var data = [
        "__v":0
     },
     {
-       "_id":"60666c42cc7b410027a1a9b2",
-       "name":"Флюоресцентная булка R2-D3",
-       "type":"bun",
-       "proteins":44,
-       "fat":26,
-       "carbohydrates":85,
-       "calories":643,
-       "price":988,
-       "image":"https://code.s3.yandex.net/react/code/bun-01.png",
-       "image_mobile":"https://code.s3.yandex.net/react/code/bun-01-mobile.png",
-       "image_large":"https://code.s3.yandex.net/react/code/bun-01-large.png",
-       "__v":0
-    }
+      "_id":"60666c42cc7b410027a1a921",
+      "name":"Краторная булка N-200i",
+      "type":"bun",
+      "proteins":80,
+      "fat":24,
+      "carbohydrates":53,
+      "calories":420,
+      "price":1255,
+      "image":"https://code.s3.yandex.net/react/code/bun-02.png",
+      "image_mobile":"https://code.s3.yandex.net/react/code/bun-02-mobile.png",
+      "image_large":"https://code.s3.yandex.net/react/code/bun-02-large.png",
+      "__v":0
+   }
  ] 
 
  export default data;
